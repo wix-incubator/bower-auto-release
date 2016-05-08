ed482becc111da24da0ce769e94d25bfd1575d12
David Perlman <davidp@wix.com>
2016-05-08 12:06:42 +0300

    fixed path to release.sh

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var workingDir = process.cwd();
 var packageJson = require(workingDir + '/package.json');
 
 require('child_process')
-  .spawn(workingDir + '/release.sh', [packageJson.version, workingDir],  {stdio: 'inherit'});
+  .spawn(__dirname + '/release.sh', [packageJson.version, workingDir],  {stdio: 'inherit'})
+  .on('error', function (x) { console.log(x) });
